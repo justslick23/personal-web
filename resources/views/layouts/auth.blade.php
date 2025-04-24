16db26f41dca60921856f31a5e6eb675726611e1
justslick23 <tokelo.foso@cbs.co.ls>
2025-04-24 12:21:53 +0200

    analytics

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -15,7 +15,7 @@
         <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
         <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
         <title>
-            Material Dashboard 3 by Creative Tim
+            Portfolio Dashboard
         </title>
     
         <!-- Fonts and icons -->
--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -18,7 +18,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
-    <title>Material Dashboard 3 by Creative Tim</title>
+    <title>            Portfolio Dashboard Auth
+    </title>
 
     <!-- Fonts and icons -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />

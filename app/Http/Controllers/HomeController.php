a54d4440256cd47673e5e8c813528ec5d5215105
justslick23 <tokelo.foso@cbs.co.ls>
2025-06-05 18:28:04 +0200

    pimg

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -137,56 +137,40 @@ class HomeController extends Controller
 
     
     
-    private function incrementTrackStatistic($track, $type)
+private function incrementTrackStatistic($track, $type)
 {
-    // Get or create the statistics record for the track
-    $statistics = $track->Songstatistics()->firstOrCreate(['song_id' => $track->id]);
+    $statistics = $track->songStatistics()->firstOrCreate(['song_id' => $track->id]);
 
-    switch ($type) {
-        case 'views':
-            $statistics->increment('views');
-            break;
-        case 'downloads':
-            $statistics->increment('downloads');
-            break;
-        case 'plays':
-            $statistics->increment('plays');
-            break;
-    }
-
-    $statistics->save();
+    match ($type) {
+        'views' => $statistics->increment('views'),
+        'downloads' => $statistics->increment('downloads'),
+        'plays' => $statistics->increment('plays'),
+        default => null,
+    };
 }
+
 public function downloadTrack($slug)
 {
-    // Try to find the song by slug
-    $track = Song::where('slug', $slug)->first();
+    $track = Song::where('slug', $slug)->first() 
+             ?? Album::where('slug', $slug)->first();
 
-    // If not found as a song, try finding it as an album
-    if (!$track) {
-        $track = Album::where('slug', $slug)->first();
-    }
-
-    // If still not found or no file path available, abort
     if (!$track || !$track->file_path) {
         abort(404, 'Track not found or file path missing.');
     }
 
-    // Increment download counter
     $this->incrementTrackStatistic($track, 'downloads');
 
-    // Construct full path to file
     $filePath = public_path($track->file_path);
 
     if (!file_exists($filePath)) {
         abort(404, 'File does not exist.');
     }
 
-    // Determine MIME type (can be extended later if needed)
-    $mimeType = 'audio/mpeg';
-
-    // Return the file for download
-    return response()->download($filePath, null, ['Content-Type' => $mimeType]);
+    return response()->download($filePath, $track->title . '.mp3', [
+        'Content-Type' => 'audio/mpeg',
+    ]);
 }
+
 
 
 // Helper function to get MIME type based on file extension
